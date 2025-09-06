88b9c58150b3d91175daf61a4d1e345399487310
PJ1HP2F <sashkrich@gmail.com>
2025-09-06 11:04:30 +0300

    first lab

---
+++ Sem5/ASP.Net/Lab1/ASP.NET Lab1/Controllers/Home.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Lab1.Controllers
+{
+    public class Home : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
